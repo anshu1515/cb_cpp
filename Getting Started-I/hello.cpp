fd8030f7e210ebca6729faccd1615d53c332413f
anshu1515 <anshubansal595@gmail.com>
2021-08-31 22:41:37 +0530

    Simple Program

---
+++ Getting Started-I/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //header file
+
+using namespace std; //standard namespace c++
+
+int main(){    //start
+
+    cout<<"hello world";    //inner statements
+
+    return 0;  //exit
+    
+}
