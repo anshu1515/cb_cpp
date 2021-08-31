8d748ec7a888ac735a05d569f7e64f140fb0c02f
anshu1515 <anshubansal595@gmail.com>
2021-08-31 22:38:43 +0530

    Break Statement

---
+++ Getting Started-I/break.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+using namespace std; 
+int main(){
+    int i=1,n;
+    cin>>n;
+    while(i<=n){
+        if(i == 5) break;
+        cout<<i<<" ";
+        i += 1;
+    }
+
+   return 0;
+}
