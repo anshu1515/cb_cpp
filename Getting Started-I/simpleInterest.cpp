0dceb1640187e5f12188814755bec7433d25f3b7
anshu1515 <anshubansal595@gmail.com>
2021-08-31 22:44:43 +0530

    Calculating Simple Interest

---
+++ Getting Started-I/simpleInterest.cpp
@@ -0,0 +1,15 @@
+#include <iostream> 
+using namespace std; 
+int main(){
+
+    int p,r,t;
+    float si;
+
+    cin>>p>>r>>t;
+
+    // si=(p*r*t)/100.0;
+    si=(p*r*t)*1.0/100;
+    cout<<si;
+    cout<<'\n';
+    return 0;
+}
