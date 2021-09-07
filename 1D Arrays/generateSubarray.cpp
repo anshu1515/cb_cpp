7d17461b6e373b2df28d3b29364581dc4a48cfb5
anshu1515 <anshubansal595@gmail.com>
2021-09-07 23:44:24 +0530

    Generate Sub-arrays

---
+++ 1D Arrays/generateSubarray.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+	int n;
+	cin>>n;
+	int arr[10000];
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+	int i,j,k;
+	for(i=0; i<n; i++){
+		for(j=i; j<n; j++){
+			for(k=i; k<=j; k++){
+				cout<<arr[k]<<" ";
+			}
+			cout<<endl;
+		}
+	}
+	return 0;
+}
